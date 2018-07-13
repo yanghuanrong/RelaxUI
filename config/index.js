d5706b17235da78512ff226d0e7984dfc45d47f2
Yan <1176989241@qq.com>
2018-07-13 17:07:00 +0800

    build

--- config/index.js
+++ config/index.js
@@ -8,8 +8,8 @@ module.exports = {
   dev: {
 
     // Paths
-    assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsSubDirectory: './static',
+    assetsPublicPath: './',
     proxyTable: {},
 
     // Various Dev Server settings
@@ -49,7 +49,7 @@ module.exports = {
 
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
-    assetsSubDirectory: 'static',
+    assetsSubDirectory: './static',
     assetsPublicPath: './',
 
     /**
