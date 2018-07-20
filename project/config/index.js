fd9d698eac7b1dfd777afb10fa449c0324b5ff31
Yan <1176989241@qq.com>
2018-07-20 17:52:52 +0800

    build

--- project/config/index.js
+++ project/config/index.js
@@ -45,12 +45,12 @@ module.exports = {
 
   build: {
     // Template for index.html
-    index: path.resolve(__dirname, '../dist/index.html'),
+    index: path.resolve(__dirname, '../../index.html'),
 
     // Paths
-    assetsRoot: path.resolve(__dirname, '../dist'),
-    assetsSubDirectory: './static',
-    assetsPublicPath: './',
+    assetsRoot: path.resolve(__dirname, '../../'),
+    assetsSubDirectory: 'static',
+    assetsPublicPath: '/',
 
     /**
      * Source Maps
