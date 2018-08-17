9c1738ee898e59afb1492bb3681536b7f80091f5
Yan <1176989241@qq.com>
2018-08-17 15:29:06 +0800

    update

--- config/index.js
+++ config/index.js
@@ -45,10 +45,10 @@ module.exports = {
 
   build: {
     // Template for index.html
-    index: path.resolve(__dirname, '../../index.html'),
+    index: path.resolve(__dirname, '../dist/index.html'),
 
     // Paths
-    assetsRoot: path.resolve(__dirname, '../../'),
+    assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
     assetsPublicPath: './',
 
