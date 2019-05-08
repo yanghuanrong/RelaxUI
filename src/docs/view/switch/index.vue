9b8c9216c2a29d9f52ec3d0a1f3207b7dbab9e85
yanghuanrong <1176989241@qq.com>
2019-05-08 18:14:36 +0800

    switch

--- src/docs/view/switch/index.vue
+++ src/docs/view/switch/index.vue
@@ -6,21 +6,115 @@
         <p class="page-title-decs">开关选择器</p>
       </div>
     </div>
-    <div class="components-select-demo">
+    <div class="components-switch-demo">
 
     <RelaxTag name="基本用法">
       <template slot="temp">
-        <x-switch></x-switch>
+        <x-switch v-model="status1" @change="change"></x-switch>
+        <x-switch v-model="status2"></x-switch>
       </template>
       <template slot="desc">
-        最简单的使用
+        切换状态时，触发事件
       </template>
       <textarea slot="code">
         <template>
-          <x-switch></x-switch>
+          <x-switch v-model="status" @change="change"></x-switch>
+          <x-switch v-model="status2"></x-switch>
+        </template>
+
+        <script>
+        export default {
+          data(){
+            return {
+              status1: false,
+              status2: true
+            }
+          },
+          methods: {
+            change(status){
+              this.$message.success('switch状态：' + status)
+            }
+          }
+        }
+        </script>
+      </textarea>
+    </RelaxTag>
+
+    <RelaxTag name="文字和图标">
+      <template slot="temp">
+        <x-switch>
+          <span slot="open">开</span>
+          <span slot="close">关</span>
+        </x-switch>
+
+        <x-switch>
+          <span slot="open">打开</span>
+          <span slot="close">关闭</span>
+        </x-switch>
+
+        <x-switch>
+          <x-icon type="x-icon-check" slot="open" />
+          <x-icon type="x-icon-x" slot="close"/>
+        </x-switch>
+      </template>
+      <template slot="desc">
+        自定义内容, 建议文字长度保持统一
+      </template>
+      <textarea slot="code">
+        <template>
+          <x-switch>
+            <span slot="open">开</span>
+            <span slot="close">关</span>
+          </x-switch>
+
+          <x-switch>
+            <span slot="open">打开</span>
+            <span slot="close">关闭</span>
+          </x-switch>
+
+          <x-switch>
+            <x-icon type="x-icon-check" slot="open" />
+            <x-icon type="x-icon-x" slot="close"/>
+          </x-switch>
+        </template>
+      </textarea>
+    </RelaxTag>
+
+    <RelaxTag name="不同色系">
+      <template slot="temp">
+        <x-switch type="primary"></x-switch>
+        <x-switch type="info"></x-switch>
+        <x-switch type="danger"></x-switch>
+        <x-switch type="success"></x-switch>
+      </template>
+      <template slot="desc">
+        提供了primary info danger success 四种颜色， 默认primary
+      </template>
+      <textarea slot="code">
+        <template slot="temp">
+          <x-switch type="primary"></x-switch>
+          <x-switch type="info"></x-switch>
+          <x-switch type="danger"></x-switch>
+          <x-switch type="success"></x-switch>
         </template>
       </textarea>
     </RelaxTag>
   </div>
   </div>
 </template>
+
+<script>
+export default {
+  data(){
+    return {
+      status1: false,
+      status2: true
+    }
+  },
+  methods: {
+    change(status){
+      this.$message.success('switch状态：' + status)
+    }
+  }
+}
+</script>
--- src/package/Switch/switch.vue
+++ src/package/Switch/switch.vue
@@ -1,26 +1,51 @@
-<template>
-  <button class="x-switch" @click="clickHandle" :class="{'x-switch-checked': isChecked}">
-    <span class="x-switch-inner"></span>
-    <div class="x-click-animating-node"></div>
-  </button>
-</template>
-
 <script>
 export default {
   name: "xSwitch",
   props: {
-    value: Boolean
+    value: Boolean,
+    type: {
+      type: String,
+      default: 'primary',
+      validator: value =>
+        [
+          'success',
+          'primary',
+          'warning',
+          'info',
+          'danger',
+        ].some(item => value === item)
+    },
   },
   data(){
     return {
-      isChecked: this.value
+      isChecked: this.value,
+      inner: ''
     }
   },
   methods: {
     clickHandle(){
       this.isChecked = !this.isChecked
       this.$emit('input', this.isChecked)
+      this.$emit('change', this.isChecked)
     }
   },
+  render(){
+    const className = {
+      class: [
+        'x-switch',
+        this.type && 'x-switch-' + this.type,
+      ]
+    }
+    this.isChecked && className.class.push('x-switch-checked')
+
+    return <button onClick={this.clickHandle} {...className}>
+    <span class="x-switch-inner">
+      <transition name="switch-move">
+        {this.isChecked ? this.$slots.open : this.$slots.close}
+      </transition>
+    </span>
+    <div class="x-click-animating-node"></div>
+  </button>
+  }
 }
 </script>
