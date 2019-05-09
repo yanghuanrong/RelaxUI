a80c2704fb3700992c9fdbe0a01684a0c86f1227
11 <841850130@qq.com>
2019-05-09 14:52:15 +0800

    datePicker组件创建

---
+++ src/docs/view/datePicker/index.vue
@@ -0,0 +1,25 @@
+<style lang="css" scoped>
+
+</style>
+
+<template>
+    <div>
+        <div class="topbar">
+            <div class="page-title-box">
+                <h4 class="page-title">DatePicer 日期选择器</h4>
+                <p class="page-title-decs">用于选择一个日期</p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+
+        }
+    }
+}
+</script>
+
